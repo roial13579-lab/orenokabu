07c217e04f6a4ada57608336ef495ae483209402
roial13579-lab <roial.13579@gmail.com>
2026-08-08 22:54:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(b"OK")
 
 def run_dummy_server():
-    server = HTTPServer(('0.0.0.0', 10000), SimpleHTTPRequestHandler)
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(('0.0.0.0', port), SimpleHTTPRequestHandler)
     server.serve_forever()
-
 threading.Thread(target=run_dummy_server, daemon=True).start()
 
 # Step 1で取得したBotのトークンをここに貼り付けます
